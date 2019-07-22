bb64db4556c4c5bbccca1d379fe64a37f49be514
Apple <apple@Apples-MacBook-Pro-10.local>
2019-07-22 12:20:23 -0700

    Initial Commit

---
+++ 01_dataTypeAndVariables.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+let string1 = "hello"
+let string2 = "there!"
+var anotherWelcome = string1 + "" + string2
+var welcome = "\(string1) \(string2)"
+var integer = 4.0
+var double = 5.0
+double * integer
+
+var a = 12
+var b = 65
+var c = 31
+var d = 98
+
+var payday = 10.25
+number * 20
+let string 3 = "Happy birthday to you!
+let string 4 = Happy birthday to you!
+let string 5 = Happy birthday dear
+
+
+
+
---
+++ 02_conditionals.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+//intention: this playgorud tests different comparison operators
+6<7
+18<10
+25<100
+100<=150
+100>=77
+
+//test == operator
+//use one equal sign to set a value to something
+//use the double equal to check if two things are equal
+12 == 10+2
+//test the != operator
+// ! means negation
+12 != 10+2
+
+//check what && does
+12 == 10+2
+12 == 10+2 && 12 == 6+6
+// the double && checks if both sides are true, if they are, it returns true
+
+
+
+
+
+// || check what they do
+12 == 10+2
+12 == 10+10
+12 == 10+2 || 12 == 10+10 // left is true but the right is false
+1+2 == || 1+2==2
+
+var dogAge = 12
+if dogAge < 2 {//checking if dog age is less than 2 which is true
+    print (You are a puppy🐶)// if the condition check is true, then do this code
+    
+} else if dog age < 6
+{print ("You are an average doggo!")}
+
+else print ("You are elderly")}
+
+ var favoriteFood = "cupcakes"
+if {favorite food == "chipotle"}
+{ print ("Your favorite food is chipotle? Really?")}
+}
