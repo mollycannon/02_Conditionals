8f0a3252a93a039999f2faf67524bcb7c7c36217
Apple <apple@Apples-MacBook-Pro-47.local>
2019-07-23 07:38:31 -0700

    End OF DAY 1

---
+++ 04_FunctionsPart2.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+//Robot Student Coding//
+func raisehand () {
+    let question = "What's your favorite color?"
+    if question == "What's your favorite color?"{
+        print ("Raise your right hand.")
+    } else {
+        print ("Don't raise your hand")
+
+}
+}
+raisehand ()
---
+++ 04_functions.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+//func walkDog (numberOfDogs : Int) -> Int {
+//  print ("Grab the leash")
+//    print ("Put the leash on the dog")
+//    print ("Open the door")
+//    print ("Go outside")
+//    print ("Do this \(numberOfDogs)")
+//    let numberOfPoopBags = numberOfDogs * 3
+//    return numberOfPoopBags
+//}
+//var totalCost = walkDog(numberOfDogs : 3) * 3
+//print (totalCost)
+
+//walkDog(numberOfDogs : 3)
+
+//func MakingCereal () {
+//    print ("pick out a bowl")
+//    print ("pour in the cereal")
+//    print ("pour out the milk")
+//    print ("grab a spoon")
+//    print ("enjoy your cereal")
+//}
+//
+//func walkDog (numberOfDogs : Int) -> Int {
+//    let lengthOfWalk = numberOfDogs * 15
+//    return lengthOfWalk
+//
+//}
+
+
+
+
+
+
+MakingCereal()
+
+func MakingTea () {
+    print ("Boil water and pour it into cup")
+    print ("Put in teabag")
+    print ("Enjoy your tea!")
+}
+
+
+
+
+
+
+
